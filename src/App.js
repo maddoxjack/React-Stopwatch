461177bbf53c6cb1531eb3e8d51f765f34b0548b
Jack Maddox <jackdmaddox@gmail.com>
2019-07-01 11:24:26 -0400

    Functioning stopwatch with basic styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Button from "./button.js";
+import Wrapper from "./wrapper.js";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    timer: 0
+  };
+
+  countdownInterval = 0;
+
+  addTime = () => {
+    let newTime = this.state.timer + 1;
+    this.setState({
+      timer: newTime
+    });
+  };
+
+  startTimer = () => {
+    this.countdownInterval = setInterval(this.addTime, 1000);
+  };
+
+  stopTimer = () => {
+    clearInterval(this.countdownInterval);
+  };
+
+  resetTimer = () => {
+    clearInterval(this.countdownInterval);
+    this.setState({
+      timer: 0
+    });
+  };
+
+  render() {
+    const { timer } = this.state;
+    return (
+      <div className="App">
+        <p>Timer: {timer}</p>
+        <Wrapper>
+          <Button clickHandler={this.startTimer} name={"Start Timer"} />
+          <Button clickHandler={this.stopTimer} name={"Stop Timer"} />
+          <Button clickHandler={this.resetTimer} name={"Reset Timer"} />
+        </Wrapper>
+      </div>
+    );
+  }
 }
 
 export default App;
